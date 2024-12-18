08f1671cd019bc701b3fe69e149b4e2e99bfd2f0
mortada1199 <murtada199815@gmail.com>
2024-12-18 10:51:06 +0200

    ADD function create

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AppoimentRequest;
+use App\Models\Appointments;
 use Illuminate\Http\Request;
 
 class AppointmentsController extends Controller
 {
-    //
+    //create a new appointments
+    public function store(AppoimentRequest $request)
+    {
+
+        //function crete 
+        $appointment = Appointments::create([
+            'patient_id' => $request->patient_id,
+            'date' => $request->date,
+            'comments' => $request->comments,
+        ]);
+
+        //send a success message
+        return response()->json([
+            'message' => 'Appointment created successfully!',
+            'appointment' => $appointment
+        ], 201);
+    }
+
+
+
+
+
+    
 }
